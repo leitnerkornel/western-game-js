5cb820d4f905bb68ac1d151959f6c637010d9459
Kornel Leitner <leitner.kornel@gmail.com>
2019-12-09 14:50:17 +0100

    Create routes for different levels

--- server.py
+++ server.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/level1')
+def first_level():
+    return render_template('first_level.html')
+
+
+@app.route('/level2')
+def second_level():
+    return render_template('second_level.html')
 
 
 if __name__ == '__main__':
