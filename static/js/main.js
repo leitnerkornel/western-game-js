5b73ece2cb7e63730b34f8e9dacadf168924c6ec
Kornél <leitner.kornel@gmail.com>
2019-12-10 21:12:56 +0100

    Change background, cursor change on the whole background

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
 console.log("Hello, this is a test, if you see JS works.")
+
+function setBackground() {
+    let background = document.createElement("img")
+    background.src = '/static/images/level_1_streetview.jpg'
+    background.classList.add("first-level-bg")
+    document.querySelector('body').appendChild(background)
+}
+
+setBackground()
