1f693066fddd673a2a92780c35c661bc07c9457d
Kornél <leitner.kornel@gmail.com>
2019-12-11 21:43:52 +0100

    Modified because of the image file renamed

--- static/js/level2.js
+++ static/js/level2.js
@@ -2,7 +2,7 @@ console.log("Hello, this is a test, if you see JS works.")
 
 function setBackground() {
     let background = document.createElement("img")
-    background.src = '/static/images/level_2_tavern.jpg'
+    background.src = '/static/images/level2.jpg'
     background.classList.add("second-level-bg")
     document.querySelector('body').appendChild(background)
 }
