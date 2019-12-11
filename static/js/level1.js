84c4c349da3e1f1b68984f1729141eca4b8999a0
Kornel Leitner <leitner.kornel@gmail.com>
2019-12-11 13:38:11 +0100

    Add one bandit

--- static/js/level1.js
+++ static/js/level1.js
@@ -7,4 +7,17 @@ function setBackground() {
     document.querySelector('body').appendChild(background)
 }
 
+function addBandit() {
+
+    /*let div = document.createElement("div")
+    div.classList.add("bandit-div")
+    document.querySelector('body').appendChild(div)*/
+    let bandit = document.createElement("img")
+    bandit.src = '/static/images/bandit04.png'
+    bandit.classList.add("bandit")
+    document.querySelector('body').appendChild(bandit)
+}
+
+
 setBackground()
+addBandit()
