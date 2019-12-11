0a52e07d5c1f5186f5b03fc5b91f491e7c16cf49
Kornél <leitner.kornel@gmail.com>
2019-12-11 21:44:34 +0100

    Characters appears many times for limited time feature done

--- static/js/level1.js
+++ static/js/level1.js
@@ -1,35 +1,53 @@
-console.log("Hello, this is a test, if you see JS works.")
+console.log("Hello, this is a test, if you see JS works.");
 
 function setBackground() {
-    let background = document.createElement("img")
-    background.src = '/static/images/level_1_streetview.jpg'
-    background.classList.add("first-level-bg")
+    let background = document.createElement("img");
+    background.src = '/static/images/level1.jpg';
+    background.classList.add("first-level-bg");
     document.querySelector('body').appendChild(background)
 }
 
 function addCharacter(file_name) {
 
-    let bandit = document.createElement("img")
-    bandit.src = `/static/images/${file_name}.png`
-    bandit.classList.add(`${file_name}`)
-    bandit.addEventListener('click', deleteAfterShoot)
-    document.querySelector('body').appendChild(bandit)
+    let bandit = document.createElement("img");
+    bandit.src = `/static/images/${file_name}.png`;
+    bandit.classList.add(`${file_name}`);
+    bandit.addEventListener('click', deleteAfterShoot);
+    document.querySelector('body').appendChild(bandit);
 }
 
 
 function deleteAfterShoot(event) {
     console.log(event);
     event.preventDefault();
-    event.toElement.remove()
+    event.toElement.remove();
 }
 
+function showCharacter(fileName, showTime, gameLength) {
+    let startTime = new Date().getTime();
+    let interval = setInterval(function () {
+        if (new Date().getTime() - startTime > `${gameLength}`) {
+            clearInterval(interval);
+            return;
+        }
+        addCharacter(`${fileName}`)
+    }, `${showTime}`);
+}
 
-setBackground()
-addCharacter("bandit01")
-addCharacter("bandit02")
-addCharacter("bandit03")
-addCharacter("bandit04")
-addCharacter("citizen01")
-addCharacter("citizen02")
-addCharacter("citizen03")
+function addEnemies(gameLength) {
+    showCharacter('bandit01', 2000, `${gameLength}`);
+    showCharacter('bandit02', 2000, `${gameLength}`);
+    showCharacter('bandit03', 2000, `${gameLength}`);
+    showCharacter('bandit04', 2000, `${gameLength}`);
+    showCharacter('citizen01', 2000, `${gameLength}`);
+    showCharacter('citizen02', 2000, `${gameLength}`);
+    showCharacter('citizen03', 2000, `${gameLength}`);
+}
 
+function main() {
+    const gameLength = 10000;
+    setBackground();
+    addEnemies(gameLength);
+}
+
+main();
