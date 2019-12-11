e62df1b00c8a4f54e4a1862af93cd017ef64c1e4
Kornel Leitner <leitner.kornel@gmail.com>
2019-12-11 11:34:10 +0100

    Create new JS files for each templates

--- static/js/level1.js
+++ static/js/level1.js
@@ -2,8 +2,8 @@ console.log("Hello, this is a test, if you see JS works.")
 
 function setBackground() {
     let background = document.createElement("img")
-    background.src = '/static/images/desert_menu.jpg'
-    background.classList.add("index-bg")
+    background.src = '/static/images/level_1_streetview.jpg'
+    background.classList.add("first-level-bg")
     document.querySelector('body').appendChild(background)
 }
 
--- static/js/level2.js
+++ static/js/level2.js
@@ -2,8 +2,8 @@ console.log("Hello, this is a test, if you see JS works.")
 
 function setBackground() {
     let background = document.createElement("img")
-    background.src = '/static/images/desert_menu.jpg'
-    background.classList.add("index-bg")
+    background.src = '/static/images/level_2_tavern.jpg'
+    background.classList.add("second-level-bg")
     document.querySelector('body').appendChild(background)
 }
 
