5b92a0c791863e4ad446183d721e5d62abd1de3d
Kornel Leitner <leitner.kornel@gmail.com>
2019-12-12 14:38:42 +0100

    Count hit feature completed

--- static/js/level1.js
+++ static/js/level1.js
@@ -7,15 +7,28 @@ function setBackground() {
     document.querySelector('body').appendChild(background)
 }
 
-function addCharacter(file_name) {
-
-    let bandit = document.createElement("img");
-    bandit.src = `/static/images/${file_name}.png`;
-    bandit.classList.add(`${file_name}`);
-    bandit.addEventListener('click', deleteAfterShoot);
-    document.querySelector('body').appendChild(bandit);
+function addCharacter(file_name, point) {
+    let character = document.createElement("img");
+    character.src = `/static/images/${file_name}.png`;
+    character.classList.add(`${file_name}`);
+    character.addEventListener('click', countPoints);
+    character.addEventListener('click', deleteAfterShoot);
+    document.querySelector('body').appendChild(character);
 }
 
+function countPoints() {
+    let currentCharacter = this.src.split("/").pop().split(".")[0];
+    let points = getPoints('points');
+    let bandits = ["bandit01", "bandit02", "bandit03", "bandit04"];
+    if (bandits.includes(currentCharacter)) {
+        points += 1000;
+        setPoints('points', points);
+    } else {
+        points -= 2000;
+        setPoints('points', points);
+    }
+    showPoints(points);
+}
 
 function deleteAfterShoot(event) {
     console.log(event);
@@ -36,16 +49,30 @@ function showCharacter(fileName, showTime, gameLength) {
 
 function addEnemies(gameLength) {
     showCharacter('bandit01', 2000, `${gameLength}`);
-    showCharacter('bandit02', 2000, `${gameLength}`);
-    showCharacter('bandit03', 2000, `${gameLength}`);
-    showCharacter('bandit04', 2000, `${gameLength}`);
-    showCharacter('citizen01', 2000, `${gameLength}`);
-    showCharacter('citizen02', 2000, `${gameLength}`);
-    showCharacter('citizen03', 2000, `${gameLength}`);
+    showCharacter('bandit02', 3000, `${gameLength}`);
+    showCharacter('bandit03', 5000, `${gameLength}`);
+    showCharacter('bandit04', 7000, `${gameLength}`);
+    showCharacter('citizen01', 4000, `${gameLength}`);
+    showCharacter('citizen02', 5000, `${gameLength}`);
+    showCharacter('citizen03', 9000, `${gameLength}`);
+}
+
+function setPoints(json_name, point) {
+    return localStorage.setItem(json_name, JSON.stringify(point));
+}
+
+function getPoints(json_name) {
+    return JSON.parse((localStorage.getItem(json_name)));
+}
+
+function showPoints(points) {
+    let currentPoints = document.getElementById('points');
+    currentPoints.innerText = `Points: ${points}`;
 }
 
 function main() {
-    const gameLength = 10000;
+    setPoints('points', 0);
+    const gameLength = 60000;
     setBackground();
     addEnemies(gameLength);
 }
