4b58b2e13a3fb95c25b10eb63b5c972edb9078e9
Pocaklako <ccrolcsi@gmail.com>
2019-12-12 18:28:45 +0100

    about finish the work all of with the sound options and functions

--- static/js/level1.js
+++ static/js/level1.js
@@ -4,7 +4,8 @@ function setBackground() {
     let background = document.createElement("img");
     background.src = '/static/images/level1.jpg';
     background.classList.add("background");
-    document.querySelector('body').appendChild(background)
+    document.querySelector('body').appendChild(background);
+    background.addEventListener('click', missedShoot);
 }
 
 function addCharacter(file_name, point) {
@@ -13,6 +14,7 @@ function addCharacter(file_name, point) {
     character.classList.add(`${file_name}`);
     character.addEventListener('click', countPoints);
     character.addEventListener('click', deleteAfterShoot);
+    character.addEventListener('click', succesfullHit);
     document.querySelector('body').appendChild(character);
 }
 
@@ -73,7 +75,7 @@ function showPoints(points) {
 function addSoundIcon() {
 
     let soundicon = document.createElement("img");
-    soundicon.src = `/static/images/sound.png`;
+    soundicon.src = `/static/images/nosound.png`;
     soundicon.classList.add(`sound-icon`);
     document.querySelector('.div-for-sound').appendChild(soundicon);
     soundicon.addEventListener('click', changeSoundOnOff);
@@ -84,13 +86,27 @@ function changeSoundOnOff(event) {
     console.log(event);
     event.preventDefault();
     console.log(this.src.split("/").pop().split(".")[0]);
-    if (this.src.split("/").pop().split(".")[0] === "sound"){
+    if (this.src.split("/").pop().split(".")[0] === "nosound"){
+        this.setAttribute("src", "/static/images/sound.png");
+        audio.play();
+    }
+    else {
         this.setAttribute("src", "/static/images/nosound.png");
         audio.pause();
     }
-    else {
-        this.setAttribute("src", "/static/images/sound.png");
-        audio.play();
+}
+
+function missedShoot() {
+    let missedshoot = document.querySelector('.missed-sound');
+    if (document.querySelector(".sound-icon").src.split("/").pop().split(".")[0] === "sound") {
+        missedshoot.play();
+    }
+}
+
+function succesfullHit() {
+    let successhoot = document.querySelector('.succes-sound');
+    if (document.querySelector(".sound-icon").src.split("/").pop().split(".")[0] === "sound") {
+        successhoot.play()
     }
 }
 
--- static/js/level2.js
+++ static/js/level2.js
@@ -4,13 +4,14 @@ function setBackground() {
     let background = document.createElement("img");
     background.src = '/static/images/level2.jpg';
     background.classList.add("background");
-    document.querySelector('body').appendChild(background)
+    document.querySelector('body').appendChild(background);
+    background.addEventListener('click', missedShoot);
 }
 
 function addSoundIcon() {
 
     let soundicon = document.createElement("img");
-    soundicon.src = `/static/images/sound.png`;
+    soundicon.src = `/static/images/nosound.png`;
     soundicon.classList.add(`sound-icon`);
     document.querySelector('.div-for-sound').appendChild(soundicon);
     soundicon.addEventListener('click', changeSoundOnOff);
@@ -21,13 +22,27 @@ function changeSoundOnOff(event) {
     console.log(event);
     event.preventDefault();
     console.log(this.src.split("/").pop().split(".")[0]);
-    if (this.src.split("/").pop().split(".")[0] === "sound"){
+    if (this.src.split("/").pop().split(".")[0] === "nosound"){
+        this.setAttribute("src", "/static/images/sound.png");
+        audio.play();
+    }
+    else {
         this.setAttribute("src", "/static/images/nosound.png");
         audio.pause();
     }
-    else {
-        this.setAttribute("src", "/static/images/sound.png");
-        audio.play();
+}
+
+function missedShoot() {
+    let missedshoot = document.querySelector('.missed-sound');
+    if (document.querySelector(".sound-icon").src.split("/").pop().split(".")[0] === "sound") {
+        missedshoot.play();
+    }
+}
+
+function succesfullHit() {
+    let successhoot = document.querySelector('.succes-sound');
+    if (document.querySelector(".sound-icon").src.split("/").pop().split(".")[0] === "sound") {
+        successhoot.play()
     }
 }
 
